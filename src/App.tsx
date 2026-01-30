7d169a66b107d44b1789615a57197fa56422d447
hammad <hammadpk@proton.me>
2026-01-31 00:43:55 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+function Hamza(){
+    return(
+        <h1>Hamza is ghandoo</h1>
+    );
+}
+export default Hamza;
---
+++ src/main.tsx
@@ -0,0 +1,40 @@
+// import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client'
+
+const myElement=(
+    <div>
+      <table>
+        <tr>
+          <th>
+            Sr.
+          </th>
+          <th>
+            User
+          </th>
+          <th>
+            Age
+          </th>
+        </tr>
+
+         <tr>
+          <td>
+            1
+          </td>
+          <td>
+            Hammad
+          </td>
+          <td>
+            27
+          </td>
+         </tr>
+
+      </table>
+    </div>
+)
+
+createRoot(document.getElementById('root') as HTMLElement).render(
+  <>
+  <h1>#Print H1 Using creating root as HTMLElement</h1>
+  {myElement}
+  </>
+)
